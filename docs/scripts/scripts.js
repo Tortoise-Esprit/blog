48fac4da98533c652f18107c4a90d7892b7da15a
Esprit <jquma@me.com>
2025-02-09 14:13:00 +0900

    Update scripts.js

--- docs/scripts/scripts.js
+++ docs/scripts/scripts.js
@@ -1,1 +1,16 @@
+//テキストを浮かび上がらせるアニメーション
+//catchphrase に適用
+const header= document.querySelector('#header');
+
+const keyframes = {
+    opacity: [0, 1],
+    transform: ['scale(0)', 'scale(1)'],
+    translate: ['0, 50px', 0],
+};
+const options = {
+    duration: 4000,
+    easing: 'ease',
+}
+
+header.animate(keyframes, options);
 <script src="https://kit.fontawesome.com/aae9bb6c7e.js" crossorigin="anonymous"></script>
